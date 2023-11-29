93f17618c4f729a0ef442eae45278a6617f4ef79
Matevagyok2002 <robert.szaszak@kosickaakademia.sk>
2023-11-29 09:51:07 +0100

    added material modules

--- src/modules/material.module.ts
+++ src/modules/material.module.ts
@@ -1,9 +1,17 @@
 import {NgModule} from "@angular/core";
 import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 const modules = [
   MatButtonModule,
-  
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatPaginatorModule,
+
   ];
 
 @NgModule({
