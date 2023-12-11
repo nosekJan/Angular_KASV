1270dfb0bd92eeafccc953f5e394879e3e8ebbaf
Jan <128244524+nosekJan@users.noreply.github.com>
2023-12-11 10:26:36 +0100

    navbar +  sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from "./navbar/navbar.component";
+import {MaterialModule} from "../modules/material.module";
+import {SidenavComponent} from "./sidenav/sidenav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, MaterialModule, SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatSidenavModule, MatMenuModule, MatToolbarModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
+  toggleSidenav() {
+    // You can add logic here to toggle the sidenav
+  }
+
+  navigateTo(route: string) {
+    // You can add logic here to navigate to a specific route
+  }
 }
